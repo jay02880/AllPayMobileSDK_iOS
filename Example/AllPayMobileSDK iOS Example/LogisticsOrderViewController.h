96ad385d621b474e1b164a1c8c0b26c150c16a22
羊小咩 <lamb@lamb-mei.com>
2014-11-25 16:10:59 +0800

    add base Ctrl  ＆ Tab 圖像

---
+++ Example/AllPayMobileSDK iOS Example/LogisticsOrderViewController.h
@@ -0,0 +1,14 @@
+//
+//  LogisticsOrderViewController.h
+//  AllPayMobileSDK iOS Example
+//
+//  Created by 羊小咩 on 2014/11/25.
+//  Copyright (c) 2014年 羊小咩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BasePickerViewController.h"
+
+@interface LogisticsOrderViewController : BasePickerViewController
+
+@end
